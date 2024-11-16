c3f0ebca616aef42956d30d0fa6489dc7bc541e3
Kosi Mensah <kmensah@usf.edu>
2024-11-16 16:45:05 -0500

    Set up App component with initial product list

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+import ProductList from './ProductList.jsx'
+import AddProductForm from './AddProductForm.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [products, setProducts] = useState([
+    { id: 1, name: 'Soccer', price: 60, description: `A staple of sports balls. Walk to any park and field and odds are you'll find people playing with this.` },
+    { id: 2, name: 'Jersey', price: 130, description: `A representation of a piece of clothing with the players identity and their last name on the back.` },
+    { id: 3, name: 'Cleats', price: 120, description: `A unique rubber outsole is designed to generate traction and balance on the field that allows for cuts cut and head fake has total support.` },
+    { id: 4, name: 'Compression pants', price: 30, description: `While preparing for the season, compression pants are crucial. They're stretchy, comfy, and full of Sport Style.` },
+  ]);
+
+  const addProduct = (newProduct) => {
+    setProducts([...products, {...newProduct}]); {/* allows a new, added product to join the  list of products */}
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Product Dashboard</h1>
+      <ProductList products={products} /> {/* adds the list of products to this portion  */}
+      <AddProductForm addProduct={addProduct} /> {/* adds the addProduct function to our page */}
+    </div>
+  );
+};
 
-export default App
+export default App;
