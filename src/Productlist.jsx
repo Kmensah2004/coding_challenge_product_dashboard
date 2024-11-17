ba5bd6627d2de3db2624e2f6677965d63de7754d
Kosi Mensah <kmensah@usf.edu>
2024-11-16 20:49:57 -0500

    Create ProductList component to render each ProductItem

---
+++ src/Productlist.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ProductItem from './ProductItem.jsx'
+
+const ProductList = ({ products }) => {
+    return (
+        <div>
+            <h2>Product List</h2>
+            <ul>
+                {products.map((product) => ( 
+                <ProductItem key={product.id} product={product} />
+                ))} {/* lists all the products with their details in the Product List */}
+            </ul>
+        </div>
+    );
+};
+
+export default ProductList;
